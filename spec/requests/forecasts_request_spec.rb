5e14513cfbdc58d7fa2dddc5b5c92a8988badd25
Benjamin Block <bjblock@gmail.com>
2015-03-27 15:11:09 -0500

    fixes test

--- spec/requests/forecasts_request_spec.rb
+++ spec/requests/forecasts_request_spec.rb
@@ -4,10 +4,8 @@ RSpec.describe 'Forecasts requests', type: :request do
 
   describe "GET /weather/:address" do
     it "renders the correct template" do
-      Zodiac.all.each do |zodiac|
-        get "/signs/5807+S+Woodlawn+Ave"
-        expect(response).to render_template(:location)
-      end
+      get "/weather/#{@address}"
+      expect(response).to render_template(:location)
     end
   end
 end
