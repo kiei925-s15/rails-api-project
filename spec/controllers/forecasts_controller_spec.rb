e1264125737ad5f8522fabf8b538dc1e5ba03204
Benjamin Block <bjblock@gmail.com>
2015-03-27 15:09:46 -0500

    fixes test by adding address param

--- spec/controllers/forecasts_controller_spec.rb
+++ spec/controllers/forecasts_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe ForecastsController, type: :controller do
   describe "GET #location" do
     it "responds successfully" do
-      get :location
+      get :location, address: @address
       expect(response).to be_success
     end
   end
