1f9d9a241f756a1e7fb45e6b479c9eb986209298
Benjamin Block <bjblock@gmail.com>
2015-03-27 15:10:15 -0500

    makes tests more ambiguous by using regex
    sets optional exercises as pending

--- spec/features/forecasts_feature_spec.rb
+++ spec/features/forecasts_feature_spec.rb
@@ -4,45 +4,38 @@ RSpec.describe 'Forecasts feature', type: :feature do
 
   describe "GET /weather/:address" do
     before do
-      @address = '1600+Pennsylvania+Ave+NW'
-      maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{@address}"
-      stub_request(:any, maps_url).to_return(body: File.new('spec/maps_response_body.txt'), status: 200)
-
-      @lat = '38.8976757'
-      @lng = '-77.03652799999999'
-      forecasts_url = "https://api.forecast.io/forecast/1eca96eac95b937d4da06737228bb716/#{@lat},#{@lng}"
-      stub_request(:any, forecasts_url).to_return(body: File.new('spec/forecasts_response_body.txt'), status: 200)
-
       visit "/weather/#{@address}"
     end
 
-    context 'main exercise' do
+    context 'Main Exercise' do
       it "displays the content from :address params" do
-        expect(page).to have_content(@address)
+        expect(page).to have_content(/#{Regexp.quote(@address)}|#{Regexp.quote(@address.gsub('+', ' '))}/i)
       end
 
       it "displays the current temperature for the :address" do
-        temp = '46.84'
+        temp = '46'
         expect(page).to have_content(temp)
       end
 
       it "displays the next hour's outlook for the :address" do
         outlook = 'Partly Cloudy'
-        expect(page).to have_content(outlook)
+        expect(page).to have_content(/#{Regexp.quote(outlook)}/i)
       end
 
       it "displays the next day's outlook for the :address" do
         outlook = 'Mostly cloudy throughout the day.'
-        expect(page).to have_content(outlook)
+        expect(page).to have_content(/#{Regexp.quote(outlook)}/i)
       end
     end
 
-    context 'optional exercise' do
-      it "links to bootstrap" do
+    context 'Optional Exercise' do
+      it "uses bootstrap" do
+        pending 'check bootstrap implementation separately'
         expect(page).to have_content('bootstrap.min.css')
       end
 
-      it "links to google maps api" do
+      it "uses google maps api" do
+        pending 'check google maps api implementation separately'
         expect(page).to have_content('https://maps.googleapis.com/maps/api/js?v=3.exp')
       end
     end
