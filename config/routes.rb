0f6baafa943e6ec25c9861948342c8a408281133
migsalmar <m-salgado@kellogg.northwestern.edu>
2015-04-21 17:15:02 -0500

    some cleanup of unused lines

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get("/", { :controller => "pages", :action => "home" })
-  # get("/weather/:address", { :controller => "forecasts", :action => "input" })
   get("/weather/:address", { :controller => "forecasts", :action => "location"})
 end
 
