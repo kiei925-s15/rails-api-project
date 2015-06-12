bf0c0734437a421f3424d9999fa02f57aa892458
DK10 <danielklein@Daniels-MacBook-Air-2.local>
2015-06-12 18:41:17 -0500

    fixing fork

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -4,6 +4,11 @@ class ForecastsController < ApplicationController
   def location
     #render('forecasts/location')
     @address = params[:address]
+
+    @name = "Paul"
+
+    @temperature
+
   end
 
 end
